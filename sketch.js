e6b1dc49b3ca801b575c794c05b10cd3afb309c4
unknown <stevebutti07@gmail.com>
2020-11-18 20:12:43 -0500

    a

--- sketch.js
+++ sketch.js
@@ -1,9 +1,151 @@
+const World = Matter.World;
+const Engine = Matter.Engine;
+const Bodies = Matter.Bodies;
+
+var enemy,enemyIMG, Genemy,GenemyIMG, Bememy, BememyIMG, VGenemy,VGenemy, EnemyGroup,GenemyGroup,BenemyGroup,VGenemyGroup,abc;
+var plr, playerIMG;
+var randX, randY;
+var bg, bg2;
+var restart, restartIMG;
+
+var score = 0;
+var life = 3;
+var PLAY = 1;
+var END = 0;
+var Gamestate = PLAY;
+function preload(){
+  bg2 = loadImage("Img/BG.png");
+  enemyIMG = loadImage("Img/enemy.png");
+  BenemyIMG = loadImage("Img/Benemy.png");
+  VGenemyIMG = loadImage("Img/VGenemy.png");
+  GenemyIMG = loadImage("Img/Genemy.png");
+  playerIMG = loadImage("Img/Player.png");
+  restartIMG = loadImage("Img/restart.png")
+}
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(displayWidth-10,displayHeight-140);
+  engine = Engine.create();
+  world = engine.world;
+  EnemyGroup = new Group();
+  BenemyGroup = new Group();
+  GenemyGroup = new Group();
+  VGenemyGroup = new Group();
+  
+
+  bg = createSprite(950,700,displayWidth,1000);
+  bg.addImage(bg2);
+  bg.scale = 2
+  bg.x = bg.width /2;
+  bg.velocityX = -4;
+  restart = createSprite(900,575,50,50);
+  restart.addImage(restartIMG);
+  restart.scale=0.2;
+  restart.visible = false;
+  
+  plr = createSprite(500,500,100,100);
+  plr.addImage(playerIMG);
+  plr.scale = 0.05;
+  
 }
 
 function draw() {
   background(255,255,255);  
+  if(Gamestate === PLAY){
+    restart.visible = false;
+    
+    if(bg.x < 0){
+      bg.x = bg.width/2;
+    }
+
+  plr.y = mouseY;
+  plr.x = mouseX;
+  console.log(Gamestate);
+  if(frameCount %60 === 0){
+  randX = Math.round(random(50,displayWidth-60))
+  randY = Math.round(random(50,displayHeight-190))
+  enemy = createSprite(randX,randY,100,100);
+  enemy.addImage(enemyIMG);
+  EnemyGroup.add(enemy);
+  EnemyGroup.setLifetimeEach(25);
+  }
+  
+  if(frameCount %150 === 0){
+    randX = Math.round(random(50,displayWidth-60))
+    randY = Math.round(random(50,displayHeight-190))
+    Genemy = createSprite(randX,randY,50,50);
+    Genemy.addImage(GenemyIMG);
+    Genemy.scale = 0.75
+    GenemyGroup.add(Genemy);
+    GenemyGroup.setLifetimeEach(20);
+  }
+
+    if(frameCount %80 === 0){
+      randX = Math.round(random(50,displayWidth-60))
+      randY = Math.round(random(50,displayHeight-190))
+      Benemy = createSprite(randX,randY,50,50);
+      Benemy.addImage(BenemyIMG);
+      Benemy.scale = 0.5
+      BenemyGroup.add(Benemy);
+      BenemyGroup.setLifetimeEach(20);
+    }
+      
+      if(frameCount %450 === 0){
+        randX = Math.round(random(50,displayWidth-60))
+        randY = Math.round(random(50,displayHeight-190))
+        VGenemy = createSprite(randX,randY,50,50);
+        VGenemy.addImage(VGenemyIMG);
+        VGenemy.scale = 0.75
+        VGenemyGroup.add(VGenemy);
+        VGenemyGroup.setLifetimeEach(25);
+      }
+      if(EnemyGroup.isTouching(plr)){
+        EnemyGroup.setLifetimeEach(0);
+        score = score + 1
+      }
+      if(GenemyGroup.isTouching(plr)){
+        GenemyGroup.setLifetimeEach(0);
+        score = score + 3
+      }
+      if(BenemyGroup.isTouching(plr)){
+        BenemyGroup.setLifetimeEach(0);
+        life = life - 1
+      }
+      if(VGenemyGroup.isTouching(plr)){
+        VGenemyGroup.setLifetimeEach(0);
+        life = life + 1
+      }
+      if(life === 0){
+        Gamestate = END
+      console.log(Gamestate);
+      }
+
+      }
+
+
+
+  Engine.update(engine);
   drawSprites();
+  if(Gamestate === END){
+    fill(0,255,0);
+    textSize(30);
+
+    text("Game Over",800,500)
+    restart.visible = true;
+    if(mousePressedOver(restart)){
+      reset();
+    }
+  }
+  fill(0,255,0);
+  textSize(20);
+  text("Score: " + score, 100,100);
+  text("lives: " + life, 100,150);
+
+}
+function reset(){
+  
+  Gamestate = PLAY;
+  life = 3;
+  score = 0;
+  
 }
